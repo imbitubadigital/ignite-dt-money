f7be163a728f54c891abfa2bca979bab6fca62e8
Antonio Fernandes <imbitubadigital@gmail.com>
2021-03-15 21:36:55 -0300

    estrutura da aplicação

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GlobalStyles } from './styles/globals';
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello Word</h1>
+      <GlobalStyles />
+    </div>
+  );
+}
+
